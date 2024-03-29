4336b0e89390ef8c5bc8063c845d89021c4e6a59
AlviGeo <alvigeovan29@gmail.com>
2024-03-29 23:41:08 +0700

    fix: update new sql and postman file

--- backend-nodejs.sql
+++ backend-nodejs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 28, 2024 at 04:41 AM
+-- Generation Time: Mar 29, 2024 at 05:40 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -29,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Books` (
   `id` int(11) NOT NULL,
-  `book_title` varchar(255) DEFAULT NULL,
-  `book_author` varchar(255) DEFAULT NULL,
-  `book_description` varchar(255) DEFAULT NULL,
-  `book_year` varchar(255) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
+  `bookTitle` varchar(255) DEFAULT NULL,
+  `bookAuthor` varchar(255) DEFAULT NULL,
+  `bookDescription` varchar(255) DEFAULT NULL,
+  `bookYear` varchar(255) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -42,7 +42,7 @@ CREATE TABLE `Books` (
 -- Dumping data for table `Books`
 --
 
-INSERT INTO `Books` (`id`, `book_title`, `book_author`, `book_description`, `book_year`, `user_id`, `createdAt`, `updatedAt`) VALUES
+INSERT INTO `Books` (`id`, `bookTitle`, `bookAuthor`, `bookDescription`, `bookYear`, `userId`, `createdAt`, `updatedAt`) VALUES
 (1, 'The Great Gatsby', 'F. Scott Fitzgerald', 'The story of the fabulously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan.', '1925', NULL, '2024-03-28 03:47:58', '2024-03-28 03:47:58');
 
 -- --------------------------------------------------------
@@ -75,7 +75,7 @@ CREATE TABLE `users` (
   `id` int(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `access_token` varchar(255) DEFAULT NULL,
+  `accessToken` varchar(255) DEFAULT NULL,
   `createdAt` date NOT NULL,
   `updatedAt` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -84,9 +84,8 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `access_token`, `createdAt`, `updatedAt`) VALUES
-(3, 'alvi@gmail.com', '$2b$10$TxN9vQIRlAMX4LienRd2qulUH56chuQXU39BmqI9fa8NHxaS39NGa', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjozLCJlbWFpbCI6ImFsdmlAZ21haWwuY29tIn0sImlhdCI6MTcxMTU5MjUzNCwiZXhwIjoxNzExNjc4OTM0fQ.yPr41pI8o8XZNWWGOcofgl8gXUOe5IrplqnB_Q1BusE', '2024-03-27', '2024-03-28'),
-(4, 'alvi1@gmail.com', '$2b$10$j90XaYgspvhn3bvH4W9HI.Ss5LVr3H3.Xkv2tcoABvC43b9M6eywu', NULL, '2024-03-27', '2024-03-27');
+INSERT INTO `users` (`id`, `email`, `password`, `accessToken`, `createdAt`, `updatedAt`) VALUES
+(7, 'alvi@gmail.com', '$2b$10$p7yjd5v8RHK7s6k.JDgMquLuaolyFx29yKxIHC6o9x34q75r1/wcO', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjo3LCJlbWFpbCI6ImFsdmlAZ21haWwuY29tIn0sImlhdCI6MTcxMTcyOTI5MiwiZXhwIjoxNzExODE1NjkyfQ.TAXoFmNFda_O7AAj7bASeTGpy4EFV4Hl77yL5dNtngU', '2024-03-29', '2024-03-29');
 
 --
 -- Indexes for dumped tables
@@ -119,13 +118,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `Books`
 --
 ALTER TABLE `Books`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
